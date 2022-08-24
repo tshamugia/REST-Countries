1b2f84b338b25b5440179ebb1a439f4d1c2f45aa
tshamugia <shamugiatengo@gmail.com>
2022-08-25 00:16:48 +0400

    add detail page, API call

--- src/api/http.js
+++ src/api/http.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import { ALL_COUNTRIES } from "../api/config";
+import axios from "axios";
+import { ALL_COUNTRIES, COUNTRY_DETAIL } from "../api/config";
 
+export const fetchCountries = async () => {
+  const response = await axios.get(ALL_COUNTRIES);
+  return response.data;
+};
 
-export const fetchCountries = async ()=>{
- 
-        const response = await axios.get(ALL_COUNTRIES);
-        
-        return response.data;
-   
-
-}
+export const fetchDetails = async (country) => {
+  const response = await axios.get(COUNTRY_DETAIL(country));
+  return response.data;
+};
--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -1,22 +1,71 @@
-import Back from '../../components/back/back';
-import './detail.scss'
-import { useNavigate} from 'react-router-dom';
+import Back from "../../components/back/back";
+import "./detail.scss";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchDetails } from "../../api/http";
 
+function Detail() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  console.log(data);
 
+  const navigate = useNavigate();
+  const { country } = useParams();
+  console.log("from params", country);
 
-function Detail(){
-    const navigate = useNavigate();
-    
-    
+  useEffect(() => {
+    setLoading(true);
+    fetchDetails(country)
+      .then((data) => setData(data))
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, [country]);
 
+  return (
+    <>
+      <div className="detail__container">
+        <Back navigate={navigate} />
+      </div>
+      {error && <div className="error_detail">{error}</div>}
 
-    return <div className='detail__container'>
-        <Back navigate={navigate}/>
-        <div className='country__info'>
-            
-        </div>
-    </div>
+      {loading ? (
+        <div className="loading_detail">Loading...</div>
+      ) : (
+        data.map((item, k) => (
+          <div className="data-container" key={k}>
+            <div className="data-container__flag">
+              <img src={item.flags.png} alt={k}></img>
+            </div>
+
+            <div className="data-container__data">
+              <h1>{item.name.common}</h1>
+              <div className="data-container__data--info">
+                <ul>
+                  <li>{item.population}</li>
+                  <li>{item.region}</li>
+                  <li>{item.capital}</li>
+                  <li>{item.name.common}</li>
+                  <li>{item.status}</li>
+                </ul>
+                <ul>
+                  <li>{item.population}</li>
+                  <li>{item.region}</li>
+                  <li>{item.capital}</li>
+                  <li>{item.name.common}</li>
+                  <li>{item.status}</li>
+                </ul>
+              </div>
+              <div className="data-container__data--borders">
+                <span>Borders: </span>
+                <span>germany</span>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
+    </>
+  );
 }
-
 
 export default Detail;
